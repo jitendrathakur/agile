30119d5a12e09604801f582207b9c42a887d8e96
bolly <bolly@bolly.com>
2014-10-07 11:40:59 +0530

    cakephp 2.5

---
+++ htdocs/protected/views/billingreports/index.php
@@ -0,0 +1,123 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Invoice
+        <small>#007612</small>
+    </h1>
+</section>
+
+
+<!-- Main content -->
+<section class="content invoice">
+    <!-- title row -->
+    <div class="row">
+        <div class="col-xs-12">
+            <h2 class="page-header">
+                <img src="img/logo.png" alt="User Image" width="41" height="41" class="img-circle" />
+                Goodfellow Occupational Therapy
+                <small class="pull-right">Date: 2/10/2014</small>
+            </h2>
+        </div><!-- /.col -->
+    </div>
+    <!-- info row -->
+    <div class="row invoice-info">
+        <div class="col-sm-4 invoice-col">
+            From
+            <address>
+                Director: John Goodfellow, MOT OTR<br/>
+                California Learning Connection<br/>2505 W. Shaw Ave., Building A <br/>Fresno, CA 93711
+                <br/>
+                Phone: (559) 228-9100 <br/>Fax: (559) 228-9200
+            </address>
+        </div><!-- /.col -->
+        <div class="col-sm-4 invoice-col">
+            To
+            <address>
+                Washington Union School District<br/>
+                Attn: Carissa Edeza<br/>43 San Banancio Road<br/>Salinas, CA 93908
+            </address>
+        </div><!-- /.col -->
+        <div class="col-sm-4 invoice-col">
+            <b>Invoice #1220413</b><br/>
+            <br/>
+            <b>NBCOT#:</b> 1025030<br/>
+            <b>EIN:</b> 77-0583951<br/>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <!-- Table row -->
+    <div class="row">
+        <div class="col-xs-12 table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Service Date</th>
+                    <th>Type of Service</th>
+                    <th>Hours</th>
+                    <th>Charge</th>
+                    <th>Subtotal</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>12/05/2014</td>
+                    <td>Occupational Therapy Services</td>
+                    <td>8</td>
+                    <td>$100</td>
+                    <td>$800</td>
+                </tr>
+                <tr>
+                    <td>12/06/2014</td>
+                    <td>Occupational Therapy Services</td>
+                    <td>8</td>
+                    <td>$100</td>
+                    <td>$800</td>
+                </tr>
+                <tr>
+                    <td>12/12/2014</td>
+                    <td>Occupational Therapy Services</td>
+                    <td>8</td>
+                    <td>$100</td>
+                    <td>$800</td>
+                </tr>
+                <tr>
+                    <td>12/13/2014</td>
+                    <td>Occupational Therapy Services</td>
+                    <td>8</td>
+                    <td>$100</td>
+                    <td>$800</td>
+                </tr>
+                </tbody>
+            </table>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <div class="row">
+        <!-- accepted payments column -->
+        <div class="col-xs-6">
+
+            <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+                Occupational Therapy Service for December 2013<br/>OT done by Elizabeth Jersey, OTR/L</p>
+        </div><!-- /.col -->
+        <div class="col-xs-6">
+            <p class="lead">Date Paid: Unpaid</p>
+            <div class="table-responsive">
+                <table class="table">
+                    <tr>
+                        <th>Total:</th>
+                        <td>$3200</td>
+                    </tr>
+                </table>
+            </div>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <!-- this row will not appear when printing -->
+    <div class="row no-print">
+        <div class="col-xs-12">
+            <button class="btn btn-default" onclick="window.print();"><i class="fa fa-print"></i> Print</button>
+            <button class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Paid</button>
+            <button class="btn btn-primary pull-right" style="margin-right: 5px;"><i class="fa fa-download"></i> Generate PDF</button>
+        </div>
+    </div>
+</section><!-- /.content -->
---
+++ htdocs/protected/views/students/index.php
@@ -0,0 +1,263 @@
+<!-- Main content -->
+<section class="content">
+<!-- START ACCORDION & CAROUSEL-->
+<div class="row">
+<div class="col-md-12">
+<div class="box box-solid">
+<div class="box-header">
+    <h3 class="box-title">Occupational Therapy Treatment Notes</h3>
+</div><!-- /.box-header -->
+<div class="box-body">
+<div class="box-group" id="accordion">
+<!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                General
+            </a><i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+        <div class="box-body">
+
+            <form role="form">
+                <div class="form-group">
+                    <table width="100%">
+                        <tr>
+                            <td width="25%">
+                                <label>Date:</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="text" class="form-control pull-right" id="dp1" value="" width="50"/>
+                                </div>
+
+                            </td>
+                            <td>
+
+
+                                <label> Student</label>
+                                <select class="form-control">
+                                    <option> John Doe | DOB: 12/23/2002 | GRADE: 11 | Edison High School</option>
+                                    <option> Jane Doe | DOB: 02/12/2004 | GRADE: 10 | McClane High Shcool</option>
+                                    <option> John Jane | DOB: 05/15/2008 | GRADE: 1 | Vineland Elementary</option>
+                                </select>
+
+
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+
+                                <label> IEP Due On:</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="text" class="form-control pull-right" id="dp2" value="" width="50"/>
+                                </div>
+
+
+                            </td>
+                            <td>
+
+
+                                <label> OT Services on IEP:</label>
+                                <input type="text" class="form-control" placeholder=""/>
+
+
+                            </td>
+                        </tr>
+
+                    </table>
+
+
+                </div><!-- /.form group -->
+
+                <!-- text input -->
+
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                Gross Motor / Postural
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+        <div class="box-body">
+            <!-- checkbox -->
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Sitting Posture / Balance
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Standing Balance
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Shoulder Girdle Stability
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Trunk Strength
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Range of Motion
+                </label>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"/>
+                    Neurodevelopment (NDT)
+                </label>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                Fine and Visual Motor
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+        <div class="box-body">
+
+            <div class="checkbox"><label><input type="checkbox"/> Grasping / Prehension</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Hand Strength</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Coloring / Writing / Drawing</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Scissor Skills</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Bilateral UE Coordination</label></div>
+
+        </div>
+    </div>
+</div>
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                Self Care Skills
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse">
+        <div class="box-body">
+            <div class="checkbox"><label><input type="checkbox"/> Feeding Skills / Oral Motor</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Fasteners (buttons, snaps, etc.)</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Management: School Materials</label></div>
+        </div>
+    </div>
+</div>
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
+                Sensory Skills
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse">
+        <div class="box-body">
+            <div class="checkbox"><label><input type="checkbox"/> Tactile</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Visual Perceptual</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Vestibular</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Proprioceptive / Heavy work</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Motor Planning / Praxis</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Alert Program (Self Regulation)</label></div>
+            <div class="checkbox"><label><input type="checkbox"/> Attention</label></div>
+
+        </div>
+    </div>
+</div>
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix">
+                Behaviour
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseSix" class="panel-collapse collapse">
+        <div class="box-body">
+            <label> Behaviour</label>
+            <select class="form-control">
+                <option> Good</option>
+                <option> Fair</option>
+                <option> Poor</option>
+            </select>
+        </div>
+    </div>
+</div>
+
+
+<div class="panel box box-primary">
+    <div class="box-header">
+        <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">
+                Comments
+            </a> <i class="fa fa-angle-double-right"></i>
+        </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse">
+        <div class="box-body">
+            <div class='row'>
+                <div class='col-md-12'>
+                    <div class='box-body pad'>
+                        <form>
+                            <textarea class="textarea" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                        </form>
+                    </div>
+                </div><!-- /.col-->
+            </div><!-- ./row -->
+        </div>
+    </div>
+</div>
+
+
+</div><!-- /.box-body -->
+
+<div class="box-footer">
+    <button type="submit" class="btn btn-info">Submit</button> <button type="submit" class="btn btn-danger">Cancelled</button>
+</div>
+</div><!-- /.box -->
+</div><!-- /.col -->
+
+</div><!-- /.row -->
+<!-- END ACCORDION & CAROUSEL-->
+
+
+
+
+</section><!-- /.content -->
+
